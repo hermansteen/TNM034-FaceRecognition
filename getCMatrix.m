16686c21787deb332ea284407fd58af47d316d23
Sparrisu <45034851+Sparrisu@users.noreply.github.com>
2022-11-17 14:16:44 +0100

    MMMMCCCC
    Motorcykel eller MC var en stor grej på 50/60-talet, tänker att det var hunkar som Marlon Brando eller Stevie McQueen som gjorde det coolt bland tidigare rockare, läderpajen, frippan. Ni fattar.

--- getCMatrix.m
+++ getCMatrix.m
@@ -1,16 +1,29 @@
-function C = getCMatrix(images)
+function C = getCMatrix(images, m)
 %GETCMATRIX Summary of this function goes here
 %   Detailed explanation goes here
-    C = [0,0]; 
-    index = size(images);
-    index = index(2);
-for i=1:index
-    image = cell2mat(images(1,i));
-    image = rgb2ycbcr(image);
-    Cb = image(:,:,2);
-    Cr = image(:,:,3);
-    C = C + cov(double(Cb), double(Cr));
-end
-   C = C ./ index;
+    C = [0,0; 0,0]; 
+    %index = size(images);
+    %index = index(2);
+% for i=1:index
+%     image = cell2mat(images(1,i));
+%     image = rgb2ycbcr(image);
+%     Cb = double(image(:,:,2))-m(1);
+%     Cr = double(image(:,:,3))-m(2);
+%     C(1,1) = C(1,1) + mean(Cb.*Cb, 'all');
+%     C(1,2) = C(1,2) + mean(Cb.*Cr, 'all');
+%     C(2,1) = C(2,1) + mean(Cb.*Cr, 'all');
+%     C(2,2) = C(2,2) + mean(Cr.*Cr, 'all');
+%     C = C + cov(Cb, Cr);
+% end
+
+
+   %C = (C ./ index);
+images = rgb2ycbcr(images);
+Cb = double(images(:,:,2))-m(1);
+Cr = double(images(:,:,3))-m(2);
+
+C = cov(Cb, Cr);
+
+
 end
 
