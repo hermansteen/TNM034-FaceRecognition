9cbd9950e3ebad235185dc8906f7a401ad81f528
Maria <maria.brunned@gmail.com>
2022-11-18 14:20:17 +0100

    changes in find mouth

--- findMouth.m
+++ findMouth.m
@@ -1,11 +1,25 @@
 function mouth = findMouth(mouthMapped)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-mouth = mouthMapped > 90/255;
-imshow(mouth)
+mouthThresh = 230;
+numMouths = 0;
 
-stats = regionprops(mouth, 'Centroid')
-centroids = cat(1,stats.Centroid);
+while numMouths < 1
+mouthMapp = mouthMapped > mouthThresh/255;
+imshow(mouthMapp);
+mouthThresh = mouthThresh - 10;
+
+stats = regionprops(mouthMapp, 'Centroid');
+stats = struct2table(stats);
+%stats = sortrows(stats, 'descend');
+stats = table2array(stats);
+
+numMouths = length(stats);
+
+end 
+
+%centroids = cat(1,stats.Centroid);
+centroids = stats(1:2,:);
 mouth = struct("x", floor(centroids(1,1)), "y", floor(centroids(1,2)));
 end
 
