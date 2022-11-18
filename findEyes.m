7e9dd5e88791a5fa5de1ab45d330e707d239bad9
Herman Steén <herman.a.steen@gmail.com>
2022-11-18 14:20:20 +0100

    findeyes updated to incorporate variable threshold values

--- findEyes.m
+++ findEyes.m
@@ -1,11 +1,17 @@
 function eyes = findEyes(eyemapped)
 
-eyemappedB = eyemapped > 175;
+thresh = 230;
+numEyes = 0;
+while numEyes < 2
+    eyemappedB = eyemapped > thresh;
+    thresh = thresh - 10;
 
-stats = regionprops(eyemappedB, 'Centroid', 'Circularity');
-stats = struct2table(stats);
-stats = sortrows(stats, 'Circularity', 'descend');
-stats = table2array(stats)
+    stats = regionprops(eyemappedB, 'Centroid', 'Circularity');
+    stats = struct2table(stats);
+    stats = sortrows(stats, 'Circularity', 'descend');
+    stats = table2array(stats);
+    numEyes = length(stats);
+end
 
 centroids = stats(1:2,:);
 l = struct("x", floor(centroids(1,1)), "y", floor(centroids(1,2)));
