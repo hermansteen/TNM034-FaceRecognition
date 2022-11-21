e23023d3d5c9a1d7807a0b648e59f26a10923897
Maria <maria.brunned@gmail.com>
2022-11-21 09:38:39 +0100

    Update findMouth.m

--- findMouth.m
+++ findMouth.m
@@ -14,11 +14,14 @@ stats = struct2table(stats);
 %stats = sortrows(stats, 'descend');
 stats = table2array(stats);
 
-numMouths = length(stats)
+numMouths = length(stats);
 
 end 
 
-%centroids = cat(1,stats.Centroid);
+if mouthThresh == 0
+    return;
+end
+
 centroids = stats(:,:);
 mouth = struct("x", floor(centroids(1,1)), "y", floor(centroids(1,2)));
 end
