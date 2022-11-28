2eae94595f6247b2d2186f678c0cef79582f5863
Maria <maria.brunned@gmail.com>
2022-11-28 10:57:34 +0100

    Rotation
    Rotation fungerar som den ska

--- normalize.m
+++ normalize.m
@@ -2,10 +2,39 @@ function normImage = normalize(img, eyes, mouth)
 %NORMALIZE Summary of this function goes here
 %   Detailed explanation goes here
 
+if eyes.l.x > eyes.r.x
+    
+    leftY = eyes.r.y;
+    rightY = eyes.l.y;
+
+    leftX = eyes.r.x;
+    rightX = eyes.l.x;
+else
+    
+    leftY = eyes.l.y;
+    rightY = eyes.r.y;
+
+    leftX = eyes.l.x;
+    rightX = eyes.r.x;
+
+end
+
+% Rotate image
+deltaX = rightX-leftX;
+hypotenuse = norm([leftX leftY] - [rightX rightY]);
 % 
-imRotate = 
+% % Angle between the eyes
+angle = rad2deg(acos(deltaX/hypotenuse));
 
 
-normImage = img;
+if leftY < rightY
+    rotateIm = imrotate(img, angle,'bicubic'); 
+else
+    rotateIm = imrotate(img, -angle,'bicubic'); 
+end
+    
+    
+
+normImage = rotateIm;
 end
 
